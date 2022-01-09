9689438bfe936ea61c883728b1b769925e00525f
John Benfer <Johnfbenfer@gmail.com>
2022-01-08 22:02:33 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Home from './components/Home';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { FIREBASE_API_KEY } from '../secret';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: "nfl-data-6af24.firebaseapp.com",
+  projectId: "nfl-data-6af24",
+  storageBucket: "nfl-data-6af24.appspot.com",
+  messagingSenderId: "841845406729",
+  appId: "1:841845406729:web:eb6abeb8b77b528d7d4cd8",
+  measurementId: "G-4SC1EG119W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          Hello World, I will conquor you...
+        </div>
+        <div>
+          Down with ESPN
+        </div>
+      </>
+    );
+  }
+}
+
+export default Home;
